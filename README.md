02b0b554572a2d6125af916e65a06d689c79ac50
Avinash Yadav <avinash.yadav@gmail.com>
2017-07-22 02:35:38 +0530

    Concurrency samples from udemy

---
+++ README.md
@@ -0,0 +1,2 @@
+# udemy-codes
+This set contains java concurrency examples based on online free code. The namings need to improve.
---
+++ Threads11-deadlock/src/demo1/App.java
@@ -0,0 +1,36 @@
+package demo1;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+        
+        final Runner runner = new Runner();
+
+        Thread t1 = new Thread(() -> {
+            try {
+                runner.firstThread();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                runner.secondThread();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+        
+        runner.finished();
+    }
+}
