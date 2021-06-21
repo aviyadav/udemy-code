3c8b966669a10d2367ee8fcd2597d915550a14da
Avinash Yadav <aviyadav@users.noreply.github.com>
2021-06-21 20:16:27 +0530

    aded executer sample

---
+++ UdemyThreadsPrj/src/main/java/simple/thread/demo/execdemo/ExecutorsExample.java
@@ -0,0 +1,24 @@
+package simple.thread.demo.execdemo;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorsExample {
+
+    public static void main(String[] args) {
+        System.out.println("Inside : " + Thread.currentThread().getName());
+        
+        System.out.println("Creating Executor Service...");
+        
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        
+        System.out.println("Creating Runnable...");
+        Runnable runnable = () -> {
+            System.out.println("Inside : " + Thread.currentThread().getName());
+        };
+        
+        System.out.println("Submit the task specified by the runnable to the executor service.");
+        
+        executorService.submit(runnable);
+    }
+}
